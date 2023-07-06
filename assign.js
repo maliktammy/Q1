9444bcafc09079905d2c6fdf801f9294cd1a6b56
maliktammy <138781315+maliktammy@users.noreply.github.com>
2023-07-07 03:52:13 +0500

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,2 @@
+var person = ("“Hello Eric, would you like to learn some Python today?”");
+console.log(person);
---
+++ assign.ts
@@ -0,0 +1,3 @@
+var person = ("“Hello Eric, would you like to learn some Python today?”")
+console.log(person);
+
